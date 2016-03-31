4907ddfc11c2025c92b1db346d05f3ba4f69a133
arsanto <arsanto@users.noreply.github.com>
2016-04-01 03:50:37 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,26 @@
+echo "deb-src http://archive.ubuntu.com/ubuntu/ vivid universe" | tee -a "/etc/apt/sources.list"
+
 apt-get update -y
+apt-get install sudo -y
+
+# youtube-upload install ffmpeg
+
 apt-get install software-properties-common -y
 add-apt-repository ppa:mc3man/trusty-media -y
 apt-get install ffmpeg -y
 apt-get install frei0r-plugins -y  
 
-wget https://bootstrap.pypa.io/ez_setup.py -O - | python
+# youtube-upload
+
+apt-get install python-pip -y 
+wget --no-check-certificate https://bootstrap.pypa.io/ez_setup.py -O - | python
 easy_install --upgrade google-api-python-client
 wget https://github.com/tokland/youtube-upload/archive/master.zip
 apt-get install unzip -y
 unzip master.zip
 cd youtube-upload-master
 python setup.py install
-
+cd ~
 
 # youtube-dl
 
